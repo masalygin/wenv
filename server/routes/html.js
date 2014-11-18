289a0a608f9f13639877ac7d221dcbf89c1b17d8
Ilya <masalygin.ilya@gmail.com>
2014-11-18 11:39:18 +0300

    get params

--- server/routes/html.js
+++ server/routes/html.js
@@ -1,37 +1,49 @@
-var fs = require('fs');
 var notifier = require('node-notifier');
 var config = require('nconf');
 var utils = require('../utils');
 var path = require('path');
 var Collector = require('fcollector');
+var url = require('url');
+var _ = require('lodash');
 
-var aliases = config.get('aliases');
 
-function replaceAliases(data) {
-	for (var key in aliases) {
-		data = data.replace(new RegExp(utils.regExpEscape(key), 'g'), aliases[key]);
-	}
+function replaceAliases(data, aliases) {
+	aliases = aliases || config.get('aliases');
+	_.each(aliases, function(val, key) {
+		data = data.replace(new RegExp(utils.regExpEscape(key), 'g'), val);
+	});
 	return data;
 }
 
 
-var smarty = config.get('smarty');
-var smartyRegExp = new RegExp('(\\{\\/?(' + smarty.join('|') + ')[^\\}]*\\})', 'g');
+var smartyRegExp = new RegExp('(\\{\\/?(' + config.get('smarty').join('|') + ')[^\\}]*\\})', 'g');
 
 function replaceSmarty(data) {
 	return data.replace(smartyRegExp, '');
 }
 
 
+var GET = config.get('get');
 
+function replaceByGet(data, query) {
+	_.each(query, function(val, key) {
+		if (GET[key]) {
+			if (GET[key][val]) {
+				data = replaceAliases(data, GET[key][val]);
+			}
+		}
+	});
+	return data;
+}
 
 
 module.exports = function (app) {
 
 
-	app.get(/^(.+\.(html|tpl))$/, function (req, res) {
+	app.get(/^.+\.(html|tpl)(\?.*)?$/, function (req, res) {
 
-		var filepath = './client/sites' + req.originalUrl;
+		var uri = url.parse(req.originalUrl);
+		var filepath = './client/sites' + uri.pathname;
 		var dir = path.dirname(filepath);
 		var isScssReady = false;
 		var isHtmlReady = false;
@@ -84,6 +96,8 @@ module.exports = function (app) {
 
 				var self = this;
 				var sub = [];
+
+				data = replaceByGet(data, req.query);
 
 				data = data.replace(/\{include\s+file="(db|global):([^"]+)"[^\}]*\}/g, function(include, resource, filename) {
 
