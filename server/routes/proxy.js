718b408af880c5134cc48e21174089ef9859bb22
Ilya <masalygin.ilya@gmail.com>
2014-11-19 15:52:12 +0300

    сохранять только если статус ответа 200

--- server/routes/proxy.js
+++ server/routes/proxy.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 var extra = require('fs-extra');
 var path = require('path');
 var request = require('request');
+var utils = require('../lib/utils')
 
 module.exports = function(app) {
 
-	app.use('/', function(req, res, next) {
+	app.use('/', function(req, res) {
 
 		var uri = url.parse(req.originalUrl);
 		var u = url.resolve('http://dumper.demojs0.oml.ru/', uri.pathname);
@@ -14,19 +15,39 @@ module.exports = function(app) {
 
 		extra.mkdirp(path.dirname(p), function(err) {
 
-			var writeStream = fs.createWriteStream(p);
+			if (err) {
+
+				utils.error(err, res);
+				return;
+			}
 
 			request
 				.get(u)
 				.on('error', function(err) {
-					console.log(err);
-					next();
-				})
-				.pipe(writeStream);
 
-			writeStream.on('finish', function() {
-				res.sendfile(p);
-			});
+					utils.error(err, res);
+
+				})
+				.on('response', function(response) {
+
+					if (response.statusCode !== 200) {
+
+						utils.error(uri.pathname + '\nNOT FOUND', res);
+						this.abort();
+
+					} else {
+
+						var writeStream = fs.createWriteStream(p);
+
+						writeStream.on('finish', function() {
+							res.sendfile(p);
+						});
+
+						this.pipe(writeStream);
+
+					}
+
+				});
 
 		});
 
