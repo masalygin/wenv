4de4e56a5d2f3c38ece87ededdfe07f4f8d16bfb
Ilya <masalygin.ilya@gmail.com>
2014-12-02 12:57:27 +0300

    подгрузка css с помощью lazyload

--- resources/static/wenv/livereload.js
+++ resources/static/wenv/livereload.js
@@ -1,8 +1,7 @@
 var io = require('./socket.io');
+var LazyLoad = require('./lazyload');
 var socket = io.connect(document.location.hostname + ':3001');
 var counter = 0;
-var focus = true;
-var pageXOffset, pageYOffset;
 
 function escape(path) {
 	return path.replace(/([\.\/])/g, '\\$1');
@@ -13,35 +12,15 @@ function relative(path) {
 	return root + path.replace(/\\/g, '/').split(root)[1];
 }
 
-window.addEventListener('focus', function() {
-	focus = true;
-});
-
-window.addEventListener('blur', function() {
-	focus = false;
-	pageXOffset = window.pageXOffset;
-	pageYOffset = window.pageYOffset;
-});
-
-setInterval(function() {
-
-	if (!focus) {
-		window.scrollTo(pageXOffset, pageYOffset);
-	}
-
-}, 10);
-
 function reloadCSS(path) {
 
 	path = relative(path);
 	var link = document.querySelector('link[href^=' + escape(path) + ']');
 	path = path + '.css?_=' + counter;
 
-	var newlink = document.createElement('link');
-	newlink.setAttribute('rel', 'stylesheet');
-	newlink.setAttribute('type', 'text/css');
-	newlink.setAttribute('href', path);
-	link.parentNode.replaceChild(newlink, link);
+	LazyLoad.css(path, function() {
+		link && link.parentNode.removeChild(link);
+	});
 
 }
 
