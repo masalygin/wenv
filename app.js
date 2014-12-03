42aa417f446e1c688910cf2290366632d8fb232f
Ilya <masalygin.ilya@gmail.com>
2014-12-03 12:01:11 +0300

    удалил секцию boot

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var http = require('http');
 var ip = require('ip');
 var path = require('path');
+var Sass = require('./lib/sass');
+var fs = require('fs-extra');
+
 
 require('./lib/server-response');
 
-global.app = express();
 
 global.WORK_DIR =  path.resolve(__dirname, '../www');
 global.CACHE_DIR = path.join(__dirname, 'cache');
@@ -16,7 +18,25 @@ global.SMARTY_COMMAND = path.join(__dirname, 'Smarty/index.php');
 global.PORT = process.env.PORT || 3000;
 global.IP = ip.address();
 
-require('./boot');
+
+global.app = express();
+app.use(require('static-favicon')());
+app.use(require('morgan')('dev'));
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded());
+app.use(require('cookie-parser')());
+
+
+app.io = require('socket.io')(require('http').Server(app));
+app.io.listen(3001);
+
+
+fs.removeSync(CACHE_DIR);
+Sass.addToCache(STATIC_DIR);
+fs.ensureDirSync(path.join(CACHE_DIR, 'templates_c'));
+fs.ensureDirSync(WORK_DIR);
+
+
 require('./routes');
 
 
