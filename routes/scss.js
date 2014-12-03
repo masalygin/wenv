ecac907200e9270fc7dbb5632c44d45fd317ee6e
Ilya <masalygin.ilya@gmail.com>
2014-12-03 10:15:35 +0300

    переписал route sass

--- routes/scss.js
+++ routes/scss.js
@@ -1,27 +1,12 @@
-var sass = require('node-sass');
 var path = require('path');
 var url = require('url');
+var Sass = require('../lib/sass');
 
-app.get(/^(.+\.scss\.css)(\?.*)?$/, function (req, res) {
+app.get(/^(.+\.scss\.css)(\?.*)?/, function (req, res) {
 
 	var uri = url.parse(req.originalUrl);
-	var cssFile = path.join(CACHE_DIR, 'site/images', uri.pathname.split('/images/')[1]);
-	var scssFile = cssFile.replace(/\.scss\.css$/, '.scss');
-
-	sass.render({
-		file: scssFile,
-		includePaths: [
-			path.join(CACHE_DIR, 'site/images'),
-			path.join(CACHE_DIR, 'g'),
-			SASS_DIR
-		],
-		success: function (data) {
-			res.header('Content-type', 'text/css');
-			res.send(data);
-		},
-		error: function(err) {
-			res.sendError(scssFile + '\n' + err);
-		}
-	});
+	var pathname = uri.pathname.replace(/\.scss\.css$/, '.scss');
+	var filename = Sass.getCachePath(pathname);
+	Sass.render(res, filename);
 
 });
