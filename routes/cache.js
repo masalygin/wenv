982413a5eb9f3c9873e3878c78f81abcc43a4dba
Илья Масалыгин <masaligin86@gmail.com>
2015-08-29 16:11:00 +0300

    dev

--- routes/cache.js
+++ routes/cache.js
@@ -5,15 +5,7 @@ var cache = require('../lib/cache');
 module.exports = handler;
 
 
-function *handler(next) {
-  var file = cache.local(this.request.path);
+function *handler() {
+  var file = yield cache(this.request.path);
   yield* send.call(this, file);
-  if (this.status !== 404) {
-    return;
-  }
-  yield cache.fetch(this.request.path);
-  yield* send.call(this, file);
-  if (!this.status) {
-    this.throw(404);
-  }
 }
